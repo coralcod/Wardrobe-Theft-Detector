7cb2518eacebab4136e71dee5a45c947a404ba15
Samuel Fish <samuelfish9@gmail.com>
2026-02-05 10:22:30 +0100

    Added time retrieval in comments. Also to test commits since this is my first time.

--- src/main.cpp
+++ src/main.cpp
@@ -32,3 +32,44 @@ void loop() {
 void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   // callback when data is sent??
 }
+
+/*
+#include <Arduino.h>
+#include <math.h>
+
+unsigned long startTime;
+unsigned long endTime;
+float timeDuration;
+bool measured;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, INPUT);
+}
+
+void loop() {
+  int buttonState = digitalRead(3);
+
+  while (buttonState == HIGH){
+    digitalWrite(2, HIGH);
+    if (measured == false){
+      startTime = millis();
+      measured = true;
+    }
+    buttonState = digitalRead(3);
+  }
+
+  if (measured ==  true){
+    endTime = millis();
+    timeDuration = endTime - startTime;
+    timeDuration = timeDuration / 1000;
+    Serial.print("Duration: ");
+    Serial.print(timeDuration);
+    Serial.println(" sec");
+  }
+
+  measured = false;
+  digitalWrite(2, LOW); 
+}
+*/
