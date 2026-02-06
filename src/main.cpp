a966e46a7024790b8158573b75b2fd61fd835dab
Samuel Fish <samuelfish9@gmail.com>
2026-02-06 13:53:21 +0100

    Complete code for transmitting switch state duration.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 #include <esp_now.h>
+#include <math.h>
 #include <WiFi.h>
 
-// MAC address of Board 2
+// MAC address of Board 2 - The Indicator (Receiver)
 uint8_t broadcastAddress[] ={0x34, 0x5F, 0x45, 0xE7, 0x58, 0x28};
 
 // message structure, must match on receiver end
 typedef struct
 {
-
+  float timeDuration;
 } struct_message;
 
 // create a message structure
@@ -16,60 +17,75 @@ struct_message myData;
 
 esp_now_peer_info_t peerInfo;
 
+// time variables
+unsigned long startTime, endTime;
+bool measured = false;
+
 // function prototypes
-void onDataSent(const uint8_t, esp_now_send_status_t);
+void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+void switchStateChange();
 
 void setup() {
-  Serial.begin(9600);
+  // Setup serial and Wi-Fi station
+  Serial.begin(115200);
   WiFi.mode(WIFI_MODE_STA);
+  Serial.print("\r\nMAC Address:\t");
   Serial.println(WiFi.macAddress());
+
+  // initialise ESP-NOW
+  if (esp_now_init() != ESP_OK){
+    Serial.println("Error initialising ESP-NOW");
+    return;
+  }
+
+  // register for send callback so we can tell if sent message was received OK
+  esp_now_register_send_cb(onDataSent);
+
+  // memcpy is simply copying MAC address into the "official" address that ESP-NOW uses
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  // add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  // interrupt for switch state change
+  attachInterrupt(digitalPinToInterrupt(4), switchStateChange, CHANGE);
 }
 
 void loop() {
-  
+  // set data
+  if (measured == true){
+    myData.timeDuration = (endTime - startTime) / 1000.0;
+
+    // send data
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+
+    // check data sent successfully
+    if (result == ESP_OK){
+      Serial.println("\nMessage sent");
+    } else {
+      Serial.println("Error sending message");
+    }
+    measured = false;
+    delay(2000);
+  }
 }
 
 void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  // callback when data is sent??
+  Serial.print("Delivery status:  ");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "SUCCESS" : "FAIL");
 }
 
-/*
-#include <Arduino.h>
-#include <math.h>
-
-unsigned long startTime;
-unsigned long endTime;
-float timeDuration;
-bool measured;
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(3, INPUT);
-}
-
-void loop() {
-  int buttonState = digitalRead(3);
-
-  while (buttonState == HIGH){
-    digitalWrite(2, HIGH);
-    if (measured == false){
-      startTime = millis();
-      measured = true;
-    }
-    buttonState = digitalRead(3);
-  }
-
-  if (measured ==  true){
+void switchStateChange(){
+  // get either start or end time depending on switch state
+  if (digitalRead(4) == HIGH){
+    startTime = millis();
+  } else {
     endTime = millis();
-    timeDuration = endTime - startTime;
-    timeDuration = timeDuration / 1000;
-    Serial.print("Duration: ");
-    Serial.print(timeDuration);
-    Serial.println(" sec");
+    measured = true;
   }
-
-  measured = false;
-  digitalWrite(2, LOW); 
 }
-*/
